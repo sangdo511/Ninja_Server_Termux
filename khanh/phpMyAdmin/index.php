24da934602d6365eb30b6bb9fcdf81908265bedc
sangdo511 <80408055+sangdo511@users.noreply.github.com>
2022-02-21 19:28:19 +0700

    Update index.php

--- khanh/phpMyAdmin/index.php
+++ khanh/phpMyAdmin/index.php
@@ -4,6 +4,6 @@ echo "<br>";
 echo "<br>";
 echo "NinjaSchoolOnTermux\r\n";
 echo "<br>";
-echo nl2br("\n Nguyễn Văn Khánh (KhanhNguyen9872) \n Github: https://github.com/khanhnguyen9872 \r\n Facebook: https://fb.me/khanh10a1");
+echo nl2br("\n Nguyễn Văn Khánh (ABC) \n Github: https://github.com/khanhnguyen9872 \r\n Facebook: https://fb.me/khanh10a1");
 
 ?>
